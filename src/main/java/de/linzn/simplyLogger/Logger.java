d37160156a089250ad5496f0d25fe82140aaf833
Niklas Linz <niklas.linz@enigmar.de>
2021-05-03 22:41:58 +0200

    update logger

--- src/main/java/de/linzn/simplyLogger/Logger.java
+++ src/main/java/de/linzn/simplyLogger/Logger.java
@@ -19,47 +19,81 @@ public class Logger {
 
     /**
      * Live Logging for commands. Only shown on console
+     *
      * @param msg Log entry
      */
-    public void LIVE(Object msg) {
-        this.log(Color.WHITE + msg + Color.RESET, LOGLEVEL.LIVE, msg.toString());
+    public synchronized void LIVE(Object msg) {
+        String formatted = formattingLogInput(msg);
+        this.log(Color.WHITE + formatted + Color.RESET, LOGLEVEL.LIVE, formatted);
     }
 
     /**
      * Debug Logging. Show debug log entries
+     *
      * @param msg Log entry
      */
-    public void DEBUG(Object msg) {
-        this.log(Color.PURPLE + msg + Color.RESET, LOGLEVEL.DEBUG, msg.toString());
+    public synchronized void DEBUG(Object msg) {
+        String formatted = formattingLogInput(msg);
+        this.log(Color.PURPLE + formatted + Color.RESET, LOGLEVEL.DEBUG, formatted);
     }
 
     /**
      * Info Logging. Show all default log entries
+     *
      * @param msg Log entry
      */
-    public void INFO(Object msg) {
-        this.log(Color.WHITE + msg + Color.RESET, LOGLEVEL.INFO, msg.toString());
+    public synchronized void INFO(Object msg) {
+        String formatted = formattingLogInput(msg);
+        this.log(Color.WHITE + formatted + Color.RESET, LOGLEVEL.INFO, formatted);
     }
 
     /**
      * Warning Logging. Show only errors and warning entries
+     *
      * @param msg Log entry
      */
-    public void WARNING(Object msg) {
-        this.log(Color.YELLOW + msg + Color.RESET, LOGLEVEL.WARNING, msg.toString());
+    public synchronized void WARNING(Object msg) {
+        String formatted = formattingLogInput(msg);
+        this.log(Color.YELLOW + formatted + Color.RESET, LOGLEVEL.WARNING, formatted);
     }
 
     /**
      * Warning Logging. Show only error log entries
+     *
      * @param msg Log entry
      */
-    public void ERROR(Object msg) {
-        this.log(Color.RED + msg + Color.RESET, LOGLEVEL.ERROR, msg.toString());
+    public synchronized void ERROR(Object msg) {
+        String formatted = formattingLogInput(msg);
+        this.log(Color.RED + formatted + Color.RESET, LOGLEVEL.ERROR, formatted);
+    }
+
+
+    private String formattingLogInput(Object input) {
+        String output;
+
+        if (input instanceof Exception) {
+            Exception e = (Exception) input;
+            output = "\n###########[ERROR IN STEM SYSTEM START]##############\n" + "Stacktrace:::" + getStackTrace(e) + "###########[ERROR IN STEM SYSTEM END]##############";
+        } else {
+            output = input.toString();
+        }
+
+        return output;
+    }
+
+    private static String getStackTrace(Exception ex) {
+        StringBuffer sb = new StringBuffer(500);
+        StackTraceElement[] st = ex.getStackTrace();
+        sb.append(ex.getClass().getName() + ": " + ex.getMessage() + "\n");
+        for (int i = 0; i < st.length; i++) {
+            sb.append("\t at " + st[i].toString() + "\n");
+        }
+        return sb.toString();
     }
 
     private void log(String msg, LOGLEVEL loglevel, String raw) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
-        String logPrefix = "["+dateFormat.format(new Date().getTime())+  "] [" + Thread.currentThread().getName() + "] ";
+        String logPrefix = "[" + dateFormat.format(new Date().getTime()) + "] [" + Thread.currentThread().getName() + "] ";
         if (shouldLogged(loglevel)) {
             System.out.println(logPrefix + msg);
             if (loglevel != LOGLEVEL.LIVE) {
@@ -77,7 +111,7 @@ public class Logger {
         logEntries.addLast(replaceChars(data));
     }
 
-    private String replaceChars(String data){
+    private String replaceChars(String data) {
         return data.replace("\u001B[37m", "")
                 .replace("\u001B[0m", "")
                 .replace("\u001B[32m", "")
@@ -148,7 +182,7 @@ public class Logger {
     }
 
     public List<String> getLastEntries(int max) {
-        if(logEntries.size() <= max){
+        if (logEntries.size() <= max) {
             return logEntries;
         } else {
             return logEntries.subList(logEntries.size() - max, logEntries.size());
