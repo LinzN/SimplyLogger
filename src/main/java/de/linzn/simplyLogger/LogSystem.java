54dcf3334fd5cb3425338a7e1def5a36ff804478
Niklas Linz <niklas.linz@enigmar.de>
2021-05-04 10:45:51 +0200

    update logger

--- src/main/java/de/linzn/simplyLogger/LogSystem.java
+++ src/main/java/de/linzn/simplyLogger/LogSystem.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
